fa1228ce284e7f446d2c8b652900c6f3d587424f
training042 Student <training042@sisu-login1.csc.fi>
2019-06-27 14:07:31 +0300

    Operator exercises done

--- programming/fortran/vecmod/vecmod.F90
+++ programming/fortran/vecmod/vecmod.F90
@@ -7,6 +7,26 @@ module vector_algebra
 
   ! TODO: overload operators needed by the parser
 
+  interface operator(+)
+     module procedure vector_sum
+  end interface operator(+)
+
+  interface operator(-)
+     module procedure vector_sub
+  end interface operator(-)
+
+  interface abs
+     module procedure vector_abs
+  end interface abs
+
+  interface operator(*)
+     module procedure vector_multpl
+  end interface operator(*)
+
+  interface operator(.x.)
+     module procedure vector_cross
+  end interface operator(.x.)
+
   ! ...
 
 contains
@@ -14,8 +34,54 @@ contains
 
   function vector_sum(v1, v2) result(v3)
     !    ...
+    implicit none
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+
+    v3 % x = v1 % x + v2 % x
+    v3 % y = v1 % y + v2 % y
+    v3 % z = v1 % z + v2 % z
+
   end function vector_sum
 
-  ! ...
+  function vector_sub(v1, v2) result(v3)
+    
+    implicit none
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+
+    v3 % x = v1 % x - v2 % x
+    v3 % y = v1 % y - v2 % y
+    v3 % z = v1 % z - v2 % z
+  end function vector_sub
+
+  function vector_multpl(v1,v2) result(v3)
+
+    implicit none
+    type(vector_t), intent(in) :: v1,v2
+    real :: v3
+
+    v3 = (v1%x * v2%x) + (v1%y * v2%y) + (v1%z * v2%z)
+  end function vector_multpl
+
+  function vector_abs(v) result(foo)
+    implicit none
+    type(vector_t), intent(in) :: v
+    real :: foo
+
+    foo = sqrt( v%x**2 + v%y**2 + v%z**2)
+
+  end function vector_abs
+
+  function vector_cross(v1,v2) result(v3)
+    implicit none
+    type(vector_t), intent(in) :: v1,v2
+    type(vector_t) :: v3
+
+    v3 % x = v1 % y * v2 % z - v1%z * v2%y
+    v3 % y = v1 % x * v2 % z - v1%z * v2%x
+    v3 % z = v1 % x * v2 % y - v1%y * v2%x
+
+  end function vector_cross
 
 end module vector_algebra
