58bd359d0963fdf23eaabbb40872bcbf200aeb57
training042 Student <training042@sisu-login4.csc.fi>
2019-07-01 20:09:13 +0300

    still working

--- heat/openmp/fortran/core.F90
+++ heat/openmp/fortran/core.F90
@@ -15,57 +15,29 @@ contains
     
     integer :: ierr
     type(mpi_datatype) :: column
-    type(mpi_request) :: req(2), uest
-    type(mpi_status) :: status
+    type(mpi_request) :: req(2), uest(2)
+    type(mpi_status) :: status(2)
 
     integer :: tag
 
     tag= parallel%rank 
 
     !From left to right
-    call mpi_sendrecv(field0%data(0,field0%ny),2+field0%nx,mpi_double_precision,parallel%nright,tag,&
-                      field0%data,2+field0%nx,mpi_double_precision,parallel%nleft,tag-1,& 
-		      mpi_comm_world, status)
+    call mpi_isend(field0%data(0,field0%ny),2+field0%nx,mpi_double_precision,parallel%nright,tag,&
+                   mpi_comm_world,uest(1),ierr)
+
+    call mpi_irecv(field0%data,2+field0%nx,mpi_double_precision,parallel%nleft,tag-1,& 
+		   mpi_comm_world, req(1), ierr)
 
     !From right to left
-    call mpi_sendrecv(field0%data(0,1),2+field0%nx,mpi_double_precision,parallel%nleft,tag,&
-                      field0%data(0,field0%ny+1),2+field0%nx,mpi_double_precision,&
-                      parallel%nright,tag+1,mpi_comm_world, status)
+    call mpi_isend(field0%data(0,1),2+field0%nx,mpi_double_precision,parallel%nleft,tag,&
+                   mpi_comm_world, uest(2), ierr)
+    call mpi_irecv(field0%data(0,field0%ny+1),2+field0%nx,mpi_double_precision,&
+                   parallel%nright,tag+1,mpi_comm_world, req(2) , ierr)
     
-    ! TODO start: implement halo exchange
 
-!    call mpi_type_vector(field0%nx+2,1,field0%nx+2, mpi_integer, column, ierr)
-!    call mpi_type_commit(column,ierr)
 
-    ! Send to left, receive from right
-!    if (parallel%rank > 0) then
-!       call mpi_isend(field0%data(1,1), 1, column,parallel%nleft,tag,mpi_comm_world,req,ierr)     
-!       call mpi_isend(field0%data(0,1), field0%nx+2,
-!    mpi_integer,parallel%nleft,tag,mpi_comm_world,uest,ierr)     
-!    end if
-!    if (parallel%rank < parallel%size-1) then
-!       call mpi_irecv(field0%data(1,int(field0%ny-1)), 1, column,parallel%nright,int(tag+1),mpi_comm_world,req,ierr)
-!       call mpi_irecv(field0%data(0,int(field0%ny+1)), field0%nx+2, mpi_integer,parallel%nright,int(tag+1),mpi_comm_world,req(1),ierr)
-!    end if
-    ! Send to right, receive from left
 
-!    if (parallel%rank < parallel%size -1) then
-!       call mpi_isend(field0%data(1,int(field0%ny-1)), 1,column,parallel%nright,tag,mpi_comm_world,req,ierr)
-!       call mpi_isend(field0%data(0,int(field0%ny)), field0%nx+2, mpi_integer,parallel%nright,tag,mpi_comm_world,uest,ierr)
-!    end if
-!    if (parallel%rank > 0) then
-!       call mpi_irecv(field0%data(1,1), 1,column,parallel%nleft,int(tag-1),mpi_comm_world,req,ierr)
-!       call mpi_irecv(field0%data(0,0), field0%nx+2, mpi_integer,parallel%nleft,int(tag-1),mpi_comm_world,req(2),ierr)
-!    end if
-  
-!    if (parallel%rank<parallel%size-1) then 
-!       call mpi_wait(req(1),status(1),ierr)
-!    end if
-!    if (parallel%rank >0) then
-!       call mpi_wait(req(2),status(2),ierr)
-!    end if
-!    call mpi_type_free(column,ierr)
-    ! TODO end
 
   end subroutine exchange
 
@@ -82,11 +54,12 @@ contains
     type(field), intent(inout) :: curr, prev
     real(dp) :: a, dt
     integer :: i, j, nx, ny
+    type(mpi_status) :: status(2)
 
     nx = curr%nx
     ny = curr%ny
 
-    do j = 1, ny
+    do j = 2, ny-1
        do i = 1, nx
           curr%data(i, j) = prev%data(i, j) + a * dt * &
                & ((prev%data(i-1, j) - 2.0 * prev%data(i, j) + &
@@ -95,6 +68,25 @@ contains
                &   prev%data(i, j+1)) / curr%dy**2)
        end do
     end do
+
+    call mpi_waitall(2,req,status)
+
+    do i = 1, nx
+       curr%data(i, 1) = prev%data(i, 1) + a * dt * &
+	     & ((prev%data(i-1, 1) - 2.0 * prev%data(i, 1) + &
+             &   prev%data(i+1, 1)) / curr%dx**2 + &
+             &  (prev%data(i, 0) - 2.0 * prev%data(i, 1) + &
+             &   prev%data(i, 0)) / curr%dy**2)
+     end do
+
+     do i = 1, nx
+       curr%data(i, ny) = prev%data(i, ny) + a * dt * &
+             & ((prev%data(i-1, ny) - 2.0 * prev%data(i, ny) + &
+             &   prev%data(i+1, ny)) / curr%dx**2 + &
+             &  (prev%data(i, ny-1) - 2.0 * prev%data(i, ny) + &
+             &   prev%data(i, ny+1)) / curr%dy**2)
+     end do
+
   end subroutine evolve
 
 end module core
